3378154f5bf05dcdb35895407e368e56486f8cc0
zyx124 <zhaoyuxin124@gmail.com>
2019-11-21 16:24:31 -0500

    initialize calculator

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] a = {1, 3, 43, 4, 5, 5};
+        System.out.println(Arrays.toString(a) instanceof String);
+    }
+}
